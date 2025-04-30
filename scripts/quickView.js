604ecdd79025472c7c52e5e923420553b0b994a9
Fernando Adolfo Cancino Cuenca <125626176+FFianquetto@users.noreply.github.com>
2025-04-30 08:12:23 -0600

    WeBack2

--- scripts/quickView.js
+++ scripts/quickView.js
@@ -1,4 +1,3 @@
-
 function openQuickView(imagen, titulo, precio) {
     const isMainPage = window.location.pathname.endsWith('index.html') || window.location.pathname.endsWith('/');
 
@@ -7,9 +6,18 @@ function openQuickView(imagen, titulo, precio) {
         imagenAjustada = '../' + imagen;
     }
     
+    // Buscar el producto en productData
+    const producto = productData.productos.find(p => p.titulo === titulo);
+    
     localStorage.setItem('quickViewImage', imagenAjustada);
     localStorage.setItem('quickViewTitle', titulo);
     localStorage.setItem('quickViewPrice', precio);
+    
+    if (producto) {
+        localStorage.setItem('quickViewDesc1', producto.descripcion1);
+        localStorage.setItem('quickViewDesc2', producto.descripcion2);
+        localStorage.setItem('quickViewDesc3', producto.descripcion3);
+    }
     
     let quickViewUrl = isMainPage ? 'pages/quickView.html' : 'quickView.html';
 
